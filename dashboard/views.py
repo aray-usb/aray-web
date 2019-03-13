be52a80702cf293b6271270e844135869c27a909
Andrés Ignacio Torres <andresitorresm@gmail.com>
2019-03-13 00:30:41 -0400

    Desarrolla vista para visualizar mockup de dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+"""
+Vistas para el Dashboard de Aray
+"""
+
+class DashboardView(generic.TemplateView):
+    """
+    Despliega el Dashboard inicial al usuario.
+    """
+
+    template_name = "dashboard/index.html"
