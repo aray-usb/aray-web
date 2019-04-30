f45d87b4daf73c437a0e14b7daa62f25169917b0
JwRicauter <ricauterjawil@gmail.com>
2019-04-29 22:29:02 -0400

    Cambio de urls en accounts para actualizarla con esta version de django

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,10 +1,22 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = "accounts"
 
 urlpatterns = [
-    path('', views.auth, name="auth"),
-    path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activate, name='activate'),
-    path('cerrar-sesion/', views.logout_view, name="logout"),
+    path(
+        '', 
+        views.auth, 
+        name="auth"
+    ),
+    re_path(
+        r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate,
+        name='activate'
+    ),
+    path(
+        'cerrar-sesion/', 
+        views.logout_view, 
+        name="logout"
+    ),
 ]
