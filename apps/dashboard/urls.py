924c8d961905eb6f74ed5c8117b7c6090f7ce5cb
JwRicauter <ricauterjawil@gmail.com>
2019-04-30 13:03:19 -0400

    Creaacion del front para el manejo de recursos, ademas se coloco font-awesome en los staticfiles para acceder a ellos sin internet

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -4,9 +4,11 @@ URLs (routing) para el dashboard de la aplicación
 
 from django.urls import path
 from . import views
+from apps.dashboard.resources_management.views import ResourcesView
 
 app_name = "dashboard"
 
 urlpatterns = [
     path('', views.DashboardView.as_view(), name="index"),
+    path('resources', ResourcesView.as_view(), name="resources_management"),
 ]
