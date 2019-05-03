bc9c73052ae58697f860c3c75acbf04ee9d3594a
Andrés Ignacio Torres <andresitorresm@gmail.com>
2019-05-03 02:18:08 -0400

    Agrega trailing slashes en URLs

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -9,8 +9,8 @@ app_name = "dashboard"
 
 urlpatterns = [
     path('', views.DashboardView.as_view(), name="index"),
-    path('assignments', include('apps.dashboard.assignments.urls')),
-    path('resources', include('apps.dashboard.resources_management.urls')),
-    path('personal', include('apps.dashboard.personal_management.urls')),
-    path('map', include('apps.dashboard.map.urls')),
+    path('assignments/', include('apps.dashboard.assignments.urls')),
+    path('resources/', include('apps.dashboard.resources_management.urls')),
+    path('personal/', include('apps.dashboard.personal_management.urls')),
+    path('map/', include('apps.dashboard.map.urls')),
 ]
