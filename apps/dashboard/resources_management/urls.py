6a95e5463db7d324f5d55467ed8fee305bae8bcb
Andrés Ignacio Torres <andresitorresm@gmail.com>
2019-05-02 21:27:05 -0400

    Anida URLs correctamente en Resource Management

--- apps/dashboard/resources_management/urls.py
+++ apps/dashboard/resources_management/urls.py
@@ -5,5 +5,5 @@ app_name = "resources_management"
 
 urlpatterns = [
     path('', views.ResourcesView.as_view(), name="resources"),
-    path('anadir/', views.AddResource.as_view(), name='add_woods'),
+    path('anadir/', views.AddResource.as_view(), name='add'),
 ]
--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -2,13 +2,12 @@
 URLs (routing) para el dashboard de la aplicación
 """
 
-from django.urls import path
+from django.urls import path, include
 from . import views
-from apps.dashboard.resources_management.views import ResourcesView
 
 app_name = "dashboard"
 
 urlpatterns = [
     path('', views.DashboardView.as_view(), name="index"),
-    path('resources', ResourcesView.as_view(), name="resources_management"),
+    path('resources', include('apps.dashboard.resources_management.urls')),
 ]
